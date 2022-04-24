446a5e1f04e2ee8c84a5398d6be3d3d054f0b1d7
Natalia-Dobranova <natalia.dobranova@gmail.com>
2022-04-24 17:04:25 +0200

    first commit

---
+++ task1.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{  
+		 double x = 14.44;
+	   	 double y = 10.56;
+		 double z = Math.Sqrt(x+y);
+		
+		Console.WriteLine(z);
+	}
+}
